453778acc7601b7c0f0992c5abff1cde38d4236e
Nayeem Rahman <muhammed.9939@gmail.com>
2019-10-14 19:26:53 +0100

    Don't use ES private class fields

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 export default class {
-  #onDispatch = () => {};
-  #next = new Promise(resolve => this.#onDispatch = resolve);
+  _onDispatch = ({argument, next}) => {};
+  _next = new Promise(resolve => this._onDispatch = resolve);
 
   /* Resolve with the argument carried in the next event. */
   async listen() {
-    return this.#next.then(({argument}) => argument);
+    return this._next.then(({argument}) => argument);
   }
 
   /* Trigger an event carrying the given argument. */
   dispatch(argument = null) {
-    const onDispatch = this.#onDispatch;
-    this.#next = new Promise(resolve => this.#onDispatch = resolve);
-    onDispatch({argument, next: this.#next});
+    const onDispatch = this._onDispatch;
+    this._next = new Promise(resolve => this._onDispatch = resolve);
+    onDispatch({argument, next: this._next});
   }
 
   /* Iterate asynchronously through the arguments carried in future events,
      starting with the next. */
   async * [Symbol.asyncIterator]() {
-    let next = this.#next;
+    let next = this._next;
     while (true) {
       const {argument, next: newNext} = await next;
       next = newNext;
